e275c7893790b9b88edf01450bdbab6238aa3eee
2005HAK <hebertkubis15@gmail.com>
2023-09-12 09:48:35 -0300

    erro no compare

--- StringLibrary/strlib.c
+++ StringLibrary/strlib.c
@@ -1,34 +1,28 @@
 #include "strlib.h"
+
+int main(){
+    
+    return 0;
+}
 // FUNCIONA
-int str_length(const char *str)
-{
+int str_length(const char *str){
     int i;
     if (str == NULL)
         return -1;
-    else
-    {
+    else{
         for (i = 0; str[i] != '\0'; i++){}
         return i;
     }
 }
-// FUNCIONA
-char *str_duplicate(const char *str)
-{
+// FUNCIONA. ERRO DE MANIPULAÇAO DE MEMORIA
+char *str_duplicate(const char *str){
     int tam = str_length(str), j;
-    char *strDuplicate = (char *)malloc((2 * tam + 1) * sizeof(char));
 
-    if (str != NULL)
-    {
-        if (strDuplicate != NULL)
-        {
-            for (j = 0; j <= (2 * tam); j++)
-            {
-                if (j < tam)
-                    strDuplicate[j] = str[j];
-                else if ((j - tam) < tam)
-                    strDuplicate[j] = str[(j - tam)];
-                else
-                    strDuplicate[j] = '\0';
+    if (str != NULL){
+        char *strDuplicate = (char *) malloc((tam + 1) * sizeof(char));
+        if (strDuplicate != NULL){
+            for (j = 0; j <= tam; j++){
+                strDuplicate[j] = str[j];
             }
         }
         return strDuplicate;
@@ -36,14 +30,11 @@ char *str_duplicate(const char *str)
     return NULL;
 }
 // FUNCIONA.
-int str_compare(const char *stra, const char *strb)
-{
+int str_compare(const char *stra, const char *strb){
     int tama = str_length(stra), tamb = str_length(strb), i;
 
-    if (tama == tamb)
-    {
-        for (i = 0; i < str_length(stra); i++)
-        {
+    if (tama == tamb){
+        for (i = 0; i < str_length(stra); i++){
             if (stra[i] < strb[i])
                 return -1;
             else if (stra[i] > strb[i])
@@ -56,17 +47,13 @@ int str_compare(const char *stra, const char *strb)
     else
         return 1;
 }
-
 // FUNCIONA.
-int str_reverse(char *str)
-{
+int str_reverse(char *str){
     int tam = str_length(str), i, cont = 1;
     char anterior;
 
-    if (str != NULL)
-    {
-        for (i = 0; i < (tam / 2); i++)
-        {
+    if (str != NULL){
+        for (i = 0; i < (tam / 2); i++){
             anterior = str[i];
             str[i] = str[tam - cont];
             str[tam - cont] = anterior;
@@ -77,16 +64,12 @@ int str_reverse(char *str)
     return 0;
 }
 // FUNCIONA.
-int str_upper(char *str)
-{
+int str_upper(char *str){
     int contUpper = 0, i;
 
-    if (str != NULL)
-    {
-        for (i = 0; i < str_length(str); i++)
-        {
-            if (str[i] > 96 && str[i] < 123)
-            {
+    if (str != NULL){
+        for (i = 0; i < str_length(str); i++){
+            if (str[i] > 96 && str[i] < 123){
                 str[i] -= 32;
                 contUpper++;
             }
@@ -96,16 +79,12 @@ int str_upper(char *str)
     return -1;
 }
 // FUNCIONA.
-int str_lower(char *str)
-{
+int str_lower(char *str){
     int contLower = 0, i;
 
-    if (str != NULL)
-    {
-        for (i = 0; i < str_length(str); i++)
-        {
-            if (str[i] > 64 && str[i] < 91)
-            {
+    if (str != NULL){
+        for (i = 0; i < str_length(str); i++){
+            if (str[i] > 64 && str[i] < 91){
                 str[i] += 32;
                 contLower++;
             }
@@ -115,24 +94,18 @@ int str_lower(char *str)
     return -1;
 }
 // FUNCIONA.
-int str_copy(char *dst, const char *src)
-{
+int str_copy(char *dst, const char *src){
     int tamDst = str_length(dst), tamSrc = str_length(src), i, ver = 0;
 
-    if (dst != NULL && src != NULL)
-    {
-        if (tamDst >= tamSrc)
-        {
-            for (i = 0; i < tamDst; i++)
-            {
+    if (dst != NULL && src != NULL){
+        if (tamDst >= tamSrc){
+            for (i = 0; i < tamDst; i++){
                 if (src[i] != '\0' && ver == 0)
                     dst[i] = src[i];
-                else if (ver == 0)
-                {
+                else if (ver == 0){
                     dst[i] = src[i];
                     ver = 1;
-                }
-                else
+                } else
                     dst[i] = ' ';
             }
             return 1;
@@ -141,17 +114,13 @@ int str_copy(char *dst, const char *src)
     }
     return -1;
 }
-// FUNCIONA
-char *str_concatenate(const char *stra, const char *strb)
-{
-    int tama = (str_length(stra) != -1) ? str_length(stra) : 0,
-        tamb = (str_length(strb) != -1) ? str_length(strb) : 0, i;
-    char *strConcat = (char *)malloc((tama + tamb + 1) * sizeof(char));
+// FUNCIONA. ERRO DE MANIPULAÇÃO DE MEMORIA
+char *str_concatenate(const char *stra, const char *strb){
+    int tama = (str_length(stra) != -1) ? str_length(stra) : 0, tamb = (str_length(strb) != -1) ? str_length(strb) : 0, i;
 
-    if (stra != NULL || strb != NULL)
-    {
-        for (i = 0; i < (tama + tamb + 1); i++)
-        {
+    if (stra != NULL || strb != NULL){
+        char *strConcat = (char *) malloc((tama + tamb + 1) * sizeof(char));
+        for (i = 0; i < (tama + tamb + 1); i++){
             if (i < tama)
                 strConcat[i] = stra[i];
             else if ((i >= tama) && ((i - tama) < tamb))
@@ -164,14 +133,11 @@ char *str_concatenate(const char *stra, const char *strb)
     return NULL;
 }
 // FUNCIONA
-int str_find_first(const char *str, const char c)
-{
+int str_find_first(const char *str, const char c){
     int tamStr = str_length(str), i;
 
-    if (str != NULL)
-    {
-        for (i = 0; i < tamStr; i++)
-        {
+    if (str != NULL){
+        for (i = 0; i < tamStr; i++){
             if (str[i] == c)
                 return i;
         }
@@ -180,14 +146,11 @@ int str_find_first(const char *str, const char c)
     return -1;
 }
 // FUNCIONA
-int str_find_last(const char *str, const char c)
-{
+int str_find_last(const char *str, const char c){
     int tamStr = str_length(str), i;
 
-    if (str != NULL)
-    {
-        for (i = (tamStr - 1); i > 0; i--)
-        {
+    if (str != NULL){
+        for (i = (tamStr - 1); i > 0; i--){
             if (str[i] == c)
                 return i;
         }
@@ -196,14 +159,11 @@ int str_find_last(const char *str, const char c)
     return -1;
 }
 // FUNCIONA
-int str_count_words(const char *str)
-{
+int str_count_words(const char *str){
     int tam = str_length(str), i, countSpace = 0;
 
-    if (str != NULL)
-    {
-        for (i = 0; i < tam; i++)
-        {
+    if (str != NULL){
+        for (i = 0; i < tam; i++){
             if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
                 countSpace++;
         }
