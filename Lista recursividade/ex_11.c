369b28990901ae862a2cf3e2210fa54fba10eafc
2005HAK <hebertkubis15@gmail.com>
2023-10-14 11:28:39 -0300

    recursividade pronto

--- Lista recursividade/ex_11.c
+++ Lista recursividade/ex_11.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void imprimeElementosInverso(int *vet, int tam){
-    
+int calculaMDC(int num1, int num2){
+    if(num2 == 0) return num1;
+    return calculaMDC(num2, num1%num2);
 }
 
 int main()
 {
-
+    printf("%i", calculaMDC(18, 20));
     return 0;
 }
