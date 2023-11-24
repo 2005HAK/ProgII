d36c56e06530ab6b6ac836eac29a0adc4eb40594
2005HAK <hebertkubis15@gmail.com>
2023-11-24 13:16:44 -0300

    tentando

--- Pilhas/main.c
+++ Pilhas/main.c
@@ -1,7 +1,19 @@
 #include "pilha.c"
 
 int main(){
-    int ** container = criaContainers(10);
-    printContainers(container, 10);
+    int i, tam = 3;
+    lista_t **containers = (lista_t**) malloc(sizeof(lista_t) * tam);
+    
+    for(i = 0; i < tam; i++){
+        containers[i] = lista_cria();
+        lista_insere_cauda(containers[i], i);
+    }
+
+    moveAAboveB(containers, 0, 1, tam);
+
+    for(i = 0; i < tam; i++){
+        printf("%i: ", i);
+        lista_imprime(containers[i]);
+    }
     return 0;
 }
