904e8e77934e1c3c8829007bc61615c66ad44f1d
2005HAK <hebertkubis15@gmail.com>
2023-11-25 20:48:04 -0300

    deveria estar pronto

--- Pilhas/main.c
+++ Pilhas/main.c
@@ -1,22 +1,47 @@
 #include "pilha.c"
+#include "string.h"
 
 int main(){
-    int i, tam = 10;
-    lista_t **containers = (lista_t**) malloc(sizeof(lista_t) * tam);
+    int i, tam = 0;
+    char comando[30];
+    char funcao[10];
+    char a[5];
+    char subfuncao[10];
+    char b[5];
+    int numa;
+    int numb;
+
+    scanf("%i", &tam);
+
+    if(tam > 0 && tam < 2500){
+        lista_t **containers = criaContainers(tam);
+
+        do{
+            scanf("%s", funcao);
+            if(strcmp(funcao, "sair") != 0){
+                scanf("%s %s %s", a, subfuncao, b);
+                numa = atoi(a);
+                numb = atoi(b);
+
+                if(strcmp(funcao, "mova") == 0){
+                    if(strcmp(subfuncao, "sobre") == 0){
+                        moveAAboveB(containers, numa, numb, tam);
+                    }else{
+                        moveAUpB(containers, numa, numb, tam);
+                    }
+                }else {
+                    if(strcmp(subfuncao, "sobre") == 0){
+                        stackingAAboveB(containers, numa, numb, tam);
+                    }else{
+                        stackingAUpB(containers, numa, numb, tam);
+                    }
+                }
+            }
+            
+        }while(strcmp(funcao, "sair") != 0);
+
+        exitContainers(containers, tam);
+    }
     
-    for(i = 0; i < tam; i++){
-        containers[i] = lista_cria();
-        lista_insere_cauda(containers[i], i);
-    }
-
-    moveAAboveB(containers, 0, 1, tam);
-    moveAAboveB(containers, 4, 0, tam);
-    moveAAboveB(containers, 5, 4, tam);
-    moveAAboveB(containers, 6, 1, tam);
-
-    for(i = 0; i < tam; i++){
-        printf("%i: ", i);
-        lista_imprime(containers[i]);
-    }
     return 0;
 }
--- Pilhas/pilha.c
+++ Pilhas/pilha.c
@@ -1,16 +1,5 @@
 #include "pilha.h"
 
-/**
- * arquivo lista.c
- *
- * Implemente neste arquivo as funções especificadas em lista.h.
- * Não faça modificações nas estruturas de nó e lista fornecidas
- * Você pode implementar funções auxiliares além das fornecidas,
- * para isso, coloque o cabeçário da função no arquivo .h e sua
- * implementação aqui, no arquivo .c. Não altere assinaturas de
- * funções, caso contrário seu código receberá nota 0,0 (zero).
- */
-
 // Estrutura de nó duplamente encadeado
 struct no
 {
@@ -27,7 +16,16 @@ struct lista
     int tamanho;  // Tamanho atual da lista
 };
 
-// Coloque a partir daqui a implementação das funções
+lista_t **criaContainers(int tam){
+    int i;
+    lista_t **containers = (lista_t**) malloc(sizeof(lista_t) * tam);
+    if(containers != NULL){
+        for (i = 0; i < tam; i++){
+            containers[i] = lista_cria();
+            lista_insere_cauda(containers[i], i);
+        }
+    } return containers;
+}
 
 lista_t *lista_cria(){
     lista_t *novaLista = (lista_t *) malloc(sizeof(lista_t));
@@ -63,57 +61,12 @@ int lista_inicializada(lista_t *l){
     return 0;
 }
 
-int lista_tamanho(lista_t *l){
-    if(lista_inicializada(l) == 1) return l->tamanho;
-    return -1;
-}
-
-int lista_info_cabeca(lista_t *l, int *dado){
-    if(dado != NULL && lista_inicializada(l) == 1){
-        if(l->tamanho != 0){
-            *dado = l->cabeca->info;
-            return 1;
-        } return 0;
-    } return -1;
-}
-
 int lista_info_cauda(lista_t *l, int *dado){
     if(dado != NULL && lista_inicializada(l) == 1){
         if(l->tamanho != 0){
             *dado = l->cauda->info;
             return 1;
         } return 0;
-    } return -1;
-}
-
-int lista_info_posicao(lista_t *l, int *dado, int pos){
-    if(dado != NULL && lista_inicializada(l) == 1){
-        if(l->tamanho != 0 && pos >= 0 && pos < l->tamanho){
-            int i;
-            no_t *atual = l->cabeca;
-            for(i = 0; i < l->tamanho; i++){
-                if(pos == i) break;
-                atual = atual->prx;
-            }
-            *dado = atual->info;
-            return 1;
-        } return 0;
-    } return -1;
-}
-
-int lista_insere_cabeca(lista_t *l, tipo dado){
-    if(lista_inicializada(l) == 1){
-        no_t *novoNo = (no_t*) malloc(sizeof(no_t));
-        if(novoNo != NULL){
-            novoNo->info = dado;
-            novoNo->prx = l->cabeca;
-            novoNo->ant = NULL;
-            if(l->cabeca != NULL) l->cabeca->ant = novoNo;
-            l->cabeca = novoNo;
-            l->tamanho++;
-            if(l->tamanho == 1) l->cauda = novoNo;
-            return 1;
-        }
     } return -1;
 }
 
@@ -130,51 +83,6 @@ int lista_insere_cauda(lista_t *l, tipo dado){
             if(l->tamanho == 1) l->cabeca = novoNo;
             return 1;
         }
-    } return -1;
-}
-
-int lista_insere_posicao(lista_t *l, tipo dado, int pos){
-    if (lista_inicializada(l) == 1){ 
-        if(pos >= 0 && pos <= l->tamanho){
-            if(pos == 0) return lista_insere_cabeca(l, dado);
-            else if (pos == l->tamanho) return lista_insere_cauda(l, dado);
-            else{
-                no_t *novoNo = (no_t *)malloc(sizeof(no_t));
-                if (novoNo != NULL){
-                    int i;
-                    no_t *atual = l->cabeca;
-                    for (i = 0; i < l->tamanho; i++){
-                        if (pos == i) break;
-                        atual = atual->prx;
-                    }
-                    novoNo->prx = atual;
-                    novoNo->ant = atual->ant;
-                    atual->ant->prx = novoNo;
-                    atual->ant = novoNo;
-                    novoNo->info = dado;
-                    l->tamanho++;
-                } else return -1;
-            } return 1;
-        } return 0;
-    } return -1;
-}
-
-int lista_remove_cabeca(lista_t *l, tipo *dado){
-    if (dado != NULL && lista_inicializada(l) == 1){ 
-        if(l->tamanho != 0){
-            if(l->cabeca != NULL){
-                no_t *cabecaNova = l->cabeca->prx;
-                *dado = l->cabeca->info;
-                l->cabeca->prx = NULL;
-                free(l->cabeca);
-                l->cabeca = cabecaNova;
-                if(l->cabeca != NULL) l->cabeca->ant = NULL;
-                else l->cauda = NULL;
-                cabecaNova = NULL;
-                l->tamanho--;
-                return 1;
-            } else return -1;
-        } return 0;
     } return -1;
 }
 
@@ -197,61 +105,6 @@ int lista_remove_cauda(lista_t *l, tipo *dado){
     } return -1;
 }
 
-int lista_remove_posicao(lista_t *l, tipo *dado, int pos){
-    if (dado != NULL && lista_inicializada(l) == 1){ 
-        if(l->tamanho != 0 && pos >=0 && pos < l->tamanho){
-            if (pos == 0) lista_remove_cabeca(l, dado);
-            else if (pos == l->tamanho - 1) lista_remove_cauda(l, dado);
-            else {
-                int i;
-                no_t *atual = l->cabeca;
-                for (i = 0; i < l->tamanho; i++){
-                    if (pos == i) break;
-                    atual = atual->prx;
-                }
-                if(atual != NULL){
-                    *dado = atual->info;
-                    if(atual->ant != NULL) atual->ant->prx = atual->prx;
-                    if(atual->prx != NULL) atual->prx->ant = atual->ant;
-                    atual->prx = NULL;
-                    atual->ant = NULL;
-                    free(atual);
-                    atual = NULL;
-                    l->tamanho--;
-                } else return -1;
-            } return 1;
-        } return 0;
-    } return -1;
-}
-
-int lista_remove_primeira(lista_t *l, tipo dado){
-    if(lista_inicializada(l) == 1){
-        if(l->tamanho != 0){
-            int i;
-            no_t *atual = l->cabeca;
-            for (i = 0; i < l->tamanho; i++){
-                if (atual->info == dado){
-                    int dado;
-                    lista_remove_posicao(l, &dado, i);
-                    return i;
-                }
-                atual = atual->prx;
-            }
-        } return -2;
-    } return -1;
-}
-
-int lista_remove_todas(lista_t *l, tipo dado){
-    if(lista_inicializada(l) == 1){
-        int retorno = lista_remove_primeira(l, dado), quantidadeRemocoes = 0;
-        while(retorno != -2){
-            quantidadeRemocoes++;
-            retorno = lista_remove_primeira(l, dado);
-        }
-        return quantidadeRemocoes;
-    } return -1;
-}
-
 int lista_busca_info(lista_t *l, tipo dado){
     if(lista_inicializada(l) == 1){
         if(l->tamanho != 0){
@@ -265,105 +118,6 @@ int lista_busca_info(lista_t *l, tipo dado){
             }
         }
     } return -1;
-}
-
-int lista_frequencia_info(lista_t *l, tipo dado){
-    if(lista_inicializada(l) == 1){
-        int freq = 0;
-        if(l->tamanho != 0){
-            int i;
-            no_t *atual = l->cabeca;
-            for (i = 0; i < l->tamanho; i++){
-                if (atual->info == dado){
-                    freq++;
-                }
-                atual = atual->prx;
-            }
-        } return freq;
-    } return -1;
-}
-
-int lista_ordenada(lista_t *l){
-    if (lista_inicializada(l) == 1){
-        if(l->tamanho != 0){
-            int i;
-            no_t *atual = l->cabeca;
-            for (i = 0; i < (l->tamanho - 1); i++){
-                if(atual->info > atual->prx->info) return 0;
-                atual = atual->prx;
-            }
-        } return 1;
-    } return -1;
-}
-
-int lista_insere_ordenado(lista_t *l, tipo dado){
-    if (lista_inicializada(l) == 1){
-        if(l->tamanho != 0){
-            int i;
-            no_t *atual = (l->cabeca);
-            for (i = 0; i < l->tamanho; i++){
-                if(dado < atual->info){
-                    lista_insere_posicao(l, dado, i);
-                    break;
-                } else if (i == (l->tamanho - 1)){
-                    lista_insere_posicao(l, dado, l->tamanho);
-                    break;
-                } 
-                atual = atual->prx;
-            }
-        } else lista_insere_cabeca(l, dado);
-        return 1;
-    } return -1;
-}
-
-int lista_compara(lista_t *l1, lista_t *l2){
-    if(lista_inicializada(l1) == 1 && lista_inicializada(l2) == 1){
-        if(l1->tamanho == l2->tamanho){
-            int i;
-            no_t *atual1 = (l1->cabeca);
-            no_t *atual2 = (l2->cabeca);
-            for (i = 0; i < l1->tamanho; i++){
-                if(atual1->info != atual2->info) return 0;
-                atual1 = atual1->prx;
-                atual2 = atual2->prx;
-            }
-            return 1;
-        } return 0;
-    } else if (lista_inicializada(l1) == 0 && lista_inicializada(l2) == 0) return 1;
-    return 0;
-}
-
-int lista_reverte(lista_t *l){
-    if (lista_inicializada(l) == 1){
-        if(l->tamanho > 1){
-            int i;
-            no_t *atual = l->cabeca;
-            l->cabeca = l->cauda;
-            l->cauda = atual;
-            for (i = 0; i < l->tamanho; i++){
-                no_t *ant = atual->ant;
-                atual->ant = atual->prx;
-                atual->prx = ant;
-                atual = atual->ant;
-            }
-        }
-        return 1;
-    } return -1;
-}
-
-lista_t *lista_cria_copia(lista_t *l){
-    if(lista_inicializada(l) == 1){
-        lista_t *copiaLista = lista_cria();
-        if(copiaLista != NULL){
-            int i;
-            no_t *atual = l->cabeca;
-            for(i = 0; i < l->tamanho; i++){
-                lista_insere_cauda(copiaLista, atual->info);
-                atual = atual->prx;
-            }
-            return copiaLista;
-        }
-    } return NULL; 
 }
 
 void lista_imprime(lista_t *l){
@@ -407,4 +161,111 @@ void moveAAboveB (lista_t **c, int a, int b, int tam){
         }
     }
 
+}
+
+void moveAUpB (lista_t **c, int a, int b, int tam){
+    int valor = 0, i, pos = 0, listaB = 0, tamanho;
+    for(i = 0; i < tam; i ++){
+        pos = lista_busca_info(c[i], b);
+        if(pos != -1){
+            listaB = i;
+            break;
+        }  
+    }
+    for(i = 0; i < tam; i ++){
+        pos = lista_busca_info(c[i], a);
+        if(pos != -1){
+            tamanho = c[i]->tamanho;
+            while(tamanho > pos + 1){
+                lista_insere_cauda(c[c[i]->cauda->info], c[i]->cauda->info);
+                lista_remove_cauda(c[i], &valor);
+                tamanho--;
+            }
+            lista_remove_cauda(c[i], &valor);
+            lista_insere_cauda(c[listaB], a);
+            break;
+        }
+    }
+}
+
+void stackingAAboveB (lista_t **c, int a, int b, int tam){
+    int pos = 0, listaB = 0, valor = 0, i, tamanho, j;
+    for(i = 0; i < tam; i ++){
+        pos = lista_busca_info(c[i], b);
+        if(pos != -1){
+            listaB = i;
+            while(c[i]->tamanho - 1 > pos){
+                lista_insere_cauda(c[c[i]->cauda->info], c[i]->cauda->info);
+                lista_remove_cauda(c[i], &valor);
+            }
+            break;
+        }  
+    }
+    for(i = 0; i < tam; i ++){
+        pos = lista_busca_info(c[i], a);
+        if(pos != -1){
+            no_t *atual = c[i]->cabeca;
+            for (j = 0; j < (c[i]->tamanho - 1); j++){
+                if (atual->info == a){
+                    break;
+                }
+                atual = atual->prx;
+            }
+            tamanho = c[i]->tamanho - pos; 
+            no_t *cauda = c[i]->cauda;
+            if(atual->ant != NULL) atual->ant->prx = NULL;
+            c[i]->cauda = atual->ant;
+            c[i]->tamanho = pos;
+            if(c[i]->tamanho == 0) c[i]->cabeca = NULL;
+            atual->ant = c[listaB]->cauda;
+            if(c[listaB]->cauda != NULL) c[listaB]->cauda->prx = atual;
+            c[listaB]->cauda = cauda;
+            c[listaB]->tamanho += tamanho;
+        }
+    }
+}
+
+void stackingAUpB (lista_t **c, int a, int b, int tam){
+    int pos = 0, listaB = 0, valor = 0, i, tamanho, j;
+    for(i = 0; i < tam; i ++){
+        pos = lista_busca_info(c[i], b);
+        if(pos != -1){
+            listaB = i;
+            break;
+        }  
+    }
+    for(i = 0; i < tam; i ++){
+        pos = lista_busca_info(c[i], a);
+        if(pos != -1){
+            if(i != listaB){
+                no_t *atual = c[i]->cabeca;
+                for (j = 0; j < (c[i]->tamanho - 1); j++){
+                    if (atual->info == a){
+                        break;
+                    }
+                    atual = atual->prx;
+                }
+                tamanho = c[i]->tamanho - pos;
+                no_t *cauda = c[i]->cauda;
+                if (atual->ant != NULL) atual->ant->prx = NULL;
+                c[i]->cauda = atual->ant;
+                c[i]->tamanho = pos;
+                if (c[i]->tamanho == 0) c[i]->cabeca = NULL;
+                atual->ant = c[listaB]->cauda;
+                if (c[listaB]->cauda != NULL) c[listaB]->cauda->prx = atual;
+                c[listaB]->cauda = cauda;
+                c[listaB]->tamanho += tamanho;
+            }
+            
+        }
+    }
+}
+
+void exitContainers (lista_t **c, int tam){
+    int i;
+    for(i = 0; i < tam; i++){
+        printf("%i: ", i);
+        lista_imprime(c[i]);
+        lista_destroi(&c[i]);
+    }
 }
